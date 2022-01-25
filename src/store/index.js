8cc5dd19a736d3d5c751c280c0965719ab1154c6
zhanshichao <zhanshichao@dxy.cn>
2022-01-25 16:35:11 +0800

    feat: notes

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
-import { camelCase } from 'lodash';
+import { camelCase } from "lodash";
 
-const requireStore = require.context('@/store', true, /[a-z0-9]+\.(js)$/i);
+const requireStore = require.context("@/store", true, /[a-z0-9]+\.(js)$/i);
 const stores = {};
 requireStore.keys().forEach((fileName) => {
-  // 一级目录的index.xx文件忽略
-  if (fileName.replace(/\.\/|.\w+$/ig, '') === 'index') return;
+  // 一级目录的index.xx文件忽略（也就是本文件）
+  if (fileName.replace(/\.\/|.\w+$/gi, "") === "index") return;
   const model = requireStore(fileName).default;
-  const name = `${camelCase(fileName.replace(/\.\/|\/index\.\w+$|.\w+$/ig, ''))}Store`;
+  const name = `${camelCase(
+    fileName.replace(/\.\/|\/index\.\w+$|.\w+$/gi, "")
+  )}Store`;
   stores[name] = model;
 });
 
